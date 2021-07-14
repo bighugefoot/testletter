5b4862727fdc92546182b3106b950228f9a96bd6
kwons <minug6046@gmail.com>
2021-07-14 15:22:23 +0900

    21.07.14

--- writer/urls.py
+++ writer/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from writer import views
-
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     path('', views.index, name= 'index'),
     path('letterwrite/', views.writer, name= 'letterwrite'),
